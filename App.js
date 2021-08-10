860ea2562667dea12fbcdeb2990f035d8fe3fbd2
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-08-10 16:25:47 +0530

    refactoring - used FlastList instead of ScrollView

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
 
   const [people, setPeople] = useState([
-    { name: "Elon", key: "1" },
-    { name: "Shivaji", key: "2" },
-    { name: "Ambedkar", key: "3" },
-    { name: "Savarkar", key: "4" },
-    { name: "Netaji", key: "5" },
-    { name: "Patel", key: "6" },
-    { name: "Modi", key: "7" }
+    { name: "Elon", id: "1" },
+    { name: "Shivaji", id: "2" },
+    { name: "Ambedkar", id: "3" },
+    { name: "Savarkar", id: "4" },
+    { name: "Netaji", id: "5" },
+    { name: "Patel", id: "6" },
+    { name: "Modi", id: "7" }
   ]);
 
 
   return (
     <View style={styles.container}>
-      <ScrollView>
+
+      <FlatList
+        keyExtractor={item => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+
+      {/* <ScrollView>
         {
           people.map(item =>
-            <View key={item.key}>
+            <View id={item id}>
               <Text style={styles.item}>{item.name}</Text>
             </View>
           )
         }
-      </ScrollView>
+      </ScrollView> */}
     </View>
   );
 }
