ce8ff9800901ff68a593fa8c269827ae2b872a16
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-08-10 17:07:15 +0530

    added TouchableOpacity component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
@@ -14,6 +14,13 @@ export default function App() {
     { name: "Modi", id: "7" }
   ]);
 
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id != id)
+    })
+  };
+
 
   return (
     <View style={styles.container}>
@@ -22,7 +29,9 @@ export default function App() {
         keyExtractor={item => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
 
