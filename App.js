aef655ff9214e17194a7be75f408212560ed48da
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-08-13 18:38:31 +0530

    movies review app - WIP, added custom font usage code

--- App.js
+++ App.js
@@ -1,74 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from "./components/header";
-import TodoItem from './components/todoItem';
-import AddToDo from "./components/addToDo";
+import Home from "./screens/home";
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf")
+  });
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  const [todos, setTodos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "buy tea", key: "2" },
-    { text: "buy coke", key: "3" },
-  ])
-
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.key != key);
-    })
+  if (fontsLoaded) {
+    return (
+      <Home />
+    )
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    )
   }
 
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setTodos((prevTodos) => {
-        return [
-          { text: text, key: Math.random().toString() },
-          ...prevTodos
-        ]
-      })
-    } else {
-      Alert.alert("OOPS!", "To-do must be 3 chars long", [
-        { text: "OK", onPress: () => console.log("alert closed") }
-      ]);
-
-    }
-  }
-
-  return (
-    <TouchableWithoutFeedback
-      onPress={() => { Keyboard.dismiss(); }}
-    >
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddToDo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={todos}
-              renderItem={({ item }) =>
-                <TodoItem item={item} pressHandler={pressHandler} />
-              }
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback >
-  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
-    padding: 30,
-    //backgroundColor: 'skyblue',
-  },
-  list: {
-    flex: 1,
-    marginTop: 20,
-    //backgroundColor: 'pink',
-  }
-});
---
+++ screens/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function About() {
+
+    return (
+        <View style={styles.container}>
+            <Text>About Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    }
+})
