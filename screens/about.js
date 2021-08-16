6ca840a0c11877fb619af333e45e651719354f5b
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-08-16 11:09:42 +0530

    added global styles

--- screens/about.js
+++ screens/about.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function About() {
 
     return (
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>About Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    }
-})
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function ReviewDetails() {
 
     return (
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    }
-})
