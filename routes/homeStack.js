447d9e8560578ac5d7c38b3a568a5571234a47f3
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-08-17 10:34:59 +0530

    added few navigation options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,13 +5,26 @@ import ReviewDetails from "../screens/reviewDetails";
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: "Game Zone",
+            //headerStyle: { backgroundColor: "#eee" }
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: "Review Details",
+            //headerStyle: { backgroundColor: "#eee" }
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: "#444",
+        headerStyle: { backgroundColor: "skyblue", height: 60 }
+    }
+});
 
 export default createAppContainer(HomeStack);
